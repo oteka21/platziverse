0f98dad22508b08f128a3b637ab125f700089645
Leonardo <lfotecam@gmail.com>
2018-07-11 17:02:52 -0500

    ejecutando pruebas a la base de datos con ava

--- platsiverse-db/setup.js
+++ platsiverse-db/setup.js
@@ -1,8 +1,25 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'esta seguro de destruir la base de datos?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('nothing happend')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -19,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
